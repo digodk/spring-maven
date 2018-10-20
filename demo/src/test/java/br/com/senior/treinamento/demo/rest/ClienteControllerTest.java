3fa71da06bf01615f1be2bcd743d79e44057991a
digodk <diogodklein@gmail.com>
2018-10-20 14:15:19 -0300

    Criados dois arquivos de teste

---
+++ demo/src/test/java/br/com/senior/treinamento/demo/rest/ClienteControllerTest.java
@@ -0,0 +1,38 @@
+package br.com.senior.treinamento.demo.rest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ClienteControllerTest {
+
+  @Test
+  public void testClienteController() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCriar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAlterar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRemover() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRetornar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testBuscarClientes() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ demo/src/test/java/br/com/senior/treinamento/demo/rest/PedidoControllerTest.java
@@ -0,0 +1,59 @@
+package br.com.senior.treinamento.demo.rest;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PedidoControllerTest {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testPedidoController() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCriar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAlterar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testBuscarClientes() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testSelecionar() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRemover() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testSelecionarItens() {
+    fail("Not yet implemented");
+  }
+
+}
